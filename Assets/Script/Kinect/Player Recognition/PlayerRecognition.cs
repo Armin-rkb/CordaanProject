9e2574559eca386ce73c6db103ee3c6fd4b95bab
Armin Karimi Birgani <armin.rkb@gmail.com>
2016-12-09 15:37:36 +0100

    Imported Kinect libary. Now able to fully test Kinect.

---
+++ Assets/Script/Kinect/Player Recognition/PlayerRecognition.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRecognition : MonoBehaviour
+{
+    [SerializeField]
+    private KinectPointController kinectPointController;
+    [SerializeField]
+    private SkeletonWrapper skeletonWrapper;
+
+    void Start()
+    {
+        StartCoroutine(CheckBoneDistance());
+    }
+
+    IEnumerator CheckBoneDistance()
+    {
+        while (true)
+        {
+            /*
+            if (skeletonWrapper.trackedPlayers[skeletonWrapper.playerAmount] > 1)
+                print("The Distance is: ");
+            else
+                print("No players");
+            */
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
